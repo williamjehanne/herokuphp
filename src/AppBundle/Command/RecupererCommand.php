3b52bb18f1d45230249b58c28a5aac48992fc78f
Jordan <jordan.dullier29@gmail.com>
2015-11-02 16:07:26 +0100

    update command

--- src/AppBundle/Command/RecupererCommand.php
+++ src/AppBundle/Command/RecupererCommand.php
@@ -8,23 +8,33 @@
 
 namespace AppBundle\Command;
 
+use AppBundle\MesClasses\Pizzeria;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class RecupererCommand extends Command
+class RecupererCommand extends ContainerAwareCommand
 {
+    private $pizzeria;
+    private $redis;
+
     protected function configure()
     {
         $this
             ->setName('pizza:recuperer')
             ->setDescription("Recuprer les pizza de l'api")
         ;
+
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $this->redis = $this->getContainer()->get('snc_redis.default');
 
         $output->writeln("Recuperer les pizzas !");
+        $this->pizzeria = new Pizzeria();
+        $ensemble_pizzas = $this->pizzeria->recupererPizzas();
+        $this->redis->set('ensemble_pizzas',$ensemble_pizzas);
     }
 }
--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -21,25 +21,16 @@ class DefaultController extends Controller
 
         //print_r($ensemble_pizzas);
 
-        $redis->set('ensemble_pizzas',$ensemble_pizzas);
+
 
         foreach($ensemble_pizzas as $pizza){
             //print_r($pizza);
             //echo "\n Pizza ".$pizza->name;
-
         }
-
-
-        //$redis->set('foo','bar');
-        //$top20 = $redis->zrevrange('leaderboard', 0, 20,'WITHSCORES');
-        //$canSet = $redis->set('exclusive:onehour', 1, 'NX', 'EX', 3600);
-
 
         if($request->isMethod("post")) {
             $pizzeria->commanderPizza();
         }
-
-        // replace this example code with whatever you need
 
         return $this->render("AppBundle:Default:commande.html.twig");
     }
