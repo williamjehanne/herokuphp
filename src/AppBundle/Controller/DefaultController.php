08741d1443e72f19f45956a983931c97b6cbefdc
Jordan <jordan.dullier29@gmail.com>
2015-11-03 10:18:41 +0100

    Use PHP Redis

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Config\Definition\Exception\Exception;
 use Symfony\Component\HttpFoundation\Request;
 
 use AppBundle\MesClasses\Pizzeria;
@@ -15,21 +16,32 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        $redis = $this->get('snc_redis.default');
+        //$redis = $this->get('snc_redis.default');
+        $redis = new \Redis();
+
+        $breakerFactory = new \Ejsmont\CircuitBreaker\Factory();
+        //$circuitBreaker = $breakerFactory->getSingleApcInstance(4, 300);
+        $circuitBreaker = $breakerFactory->getRedisInstance($redis,4,300);
+
+
         $pizzeria = new Pizzeria($this->container);
 
         $ensemble_pizzas = $pizzeria->recupererPizzasREDIS();
         $json_ensemble_pizzas = json_decode($ensemble_pizzas);
-        //echo $json_ensemble_pizzas;
-        //$redis->get('ensemble_pizzas');
 
         foreach($json_ensemble_pizzas as $pizza){
-
             echo "Pizza ".$pizza->name." - ".$pizza->price."<br/> ";
         }
 
         if($request->isMethod("post")) {
-            $pizzeria->commanderPizza();
+            if( $circuitBreaker->isAvailable("commandePizza") ) {
+                try{
+                    $pizzeria->commanderPizza();
+                    $circuitBreaker->reportSuccess("commandePizza");
+                }catch (Exception $e){
+                    $circuitBreaker->reportFailure("commandePizza");
+                }
+            }
         }
 
         return $this->render("AppBundle:Default:commande.html.twig");
